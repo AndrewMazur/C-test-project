2bc69c17bc2ed4a677b4c44c4537b37c77381511
AndrewMazur <andrew.v.mazur@gmail.com>
2016-11-17 18:03:29 +0200

    test commit

---
+++ C test file.c
@@ -0,0 +1,10 @@
+//
+//  C test file.c
+//  C test project
+//
+//  Created by Andrew on 10/26/16.
+//
+//
+
+#include "C test file.h"
+printf 'Hello, World!/b'
---
+++ C test file.h
@@ -0,0 +1,14 @@
+//
+//  C test file.h
+//  C test project
+//
+//  Created by Andrew on 10/26/16.
+//
+//
+
+#ifndef C_test_file_h
+#define C_test_file_h
+
+#include <stdio.h>
+
+#endif /* C_test_file_h */
